37e47fea4f77a0ee09965c52f0a87ba487ada7b3
colinhalexander <chalexander@icloud.com>
2019-12-03 10:25:06 -0700

    add routes in /spotify router to add and remove songs from playlist, edit console.logs in catch statements to print only the error

--- routes/spotify.js
+++ routes/spotify.js
@@ -6,8 +6,35 @@ router.get('/:spotifyID/top/songs', async (req, res, next) => {
     req.params.spotifyID,
     endpoints.getTopSongs,
     null
-  ).catch(error => console.log(error))
-  res.json(response.data)
+  ).catch(error => console.log(error.response.data))
+
+  response
+    ? res.json(response.data)
+    : res.json({ error: "Unable to access top songs" })
+})
+
+router.post('/:spotifyID/:playlistID/songs', async (req, res, next) => {
+  const response = await makeSpotifyRequest(
+    req.params.spotifyID,
+    endpoints.addSongsToPlaylist(req.params.playlistID),
+    req.body
+  ).catch(error => console.log(error.response.data))
+
+  response
+    ? res.json(response.data)
+    : res.json({ error: "Unable to add song to playlist" })
+})
+
+router.delete('/:spotifyID/:playlistID/songs', async (req, res, next) => {
+  const response = await makeSpotifyRequest(
+    req.params.spotifyID,
+    endpoints.removeSongsFromPlaylist(req.params.playlistID),
+    req.body
+  ).catch(error => console.log(error.response.data))
+
+  response
+    ? res.json(response.data)
+    : res.json({ error: "Unable to remove song from playlist" })
 })
 
 module.exports = router
